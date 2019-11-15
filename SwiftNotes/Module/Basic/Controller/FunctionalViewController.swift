ed1637a5e8f295bb231e600746a6cd66b063fb1e
任涛 <ktonyreet@gmail.com>
2019-11-15 17:36:30 +0800

    添加sequence

--- SwiftNotes/Module/Basic/Controller/FunctionalViewController.swift
+++ SwiftNotes/Module/Basic/Controller/FunctionalViewController.swift
@@ -19,7 +19,9 @@ class FunctionalViewController: BaseViewController {
 
         /// Currying
         functionalCurrying()
-    }
+
+        /// sequence
+        functionalSequence()
 
 }
 
@@ -54,3 +56,47 @@ extension FunctionalViewController {
         return {$0 > comparer }
     }
 }
+
+///Sequence
+extension FunctionalViewController {
+    func functionalSequence(){
+        let arr = [1,2,3,4,5]
+        
+        for i in ReverseSequence.init(array: arr) {
+            print(i)
+        }
+    }
+}
+
+class ReverseIterator<T>: IteratorProtocol {
+    
+    typealias Element = T
+    
+    var array: [Element]
+    var currentIndex = 0
+    init(array: [Element]) {
+        self.array = array
+        currentIndex = array.count - 1
+    }
+    func next() -> Element? {
+        if currentIndex < 0 {
+            return nil
+        }else {
+            let element = array[currentIndex]
+            currentIndex -= 1
+            return element
+        }
+    }
+}
+
+struct ReverseSequence<T>: Sequence {
+    var array: [T]
+    init(array: [T]) {
+        self.array = array
+    }
+    typealias Iterator = ReverseIterator<T>
+    func makeIterator() -> ReverseIterator<T> {
+        return ReverseIterator(array: self.array)
+    }
+}
+
